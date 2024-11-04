04cfc80dd8a489e857a38cc7c85136c6dde3f701
dmitry_muffin <andima70@gmail.com>
2024-11-04 20:27:41 +0300

    create

---
+++ src/main/java/com/dmboats/dmboats_website/DmboatsWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.dmboats.dmboats_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DmboatsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmboatsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/controllers/MainPageController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        return "index";
+    }
+
+}
